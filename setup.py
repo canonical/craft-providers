4db0b624d6c7af4d05f87c50b26c681ad03f6f85
Chris Patterson <chris.patterson@canonical.com>
2021-02-11 10:32:43 -0500

    setup.py: fix URL typo
    Co-authored-by: Sergio Schvezov <sergio.schvezov@canonical.com>

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
     setup_requires=setup_requirements,
     test_suite="tests",
     tests_require=test_requirements,
-    url="https://github.com/canonical/craft_providers",
+    url="https://github.com/canonical/craft-providers",
     version="0.0.0",
     zip_safe=False,
 )
