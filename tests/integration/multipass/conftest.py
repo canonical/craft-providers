8d50619e4256b4325915bfa9673c16e597d8d4be
Chris Patterson <chris.patterson@canonical.com>
2021-03-25 19:59:44 -0400

    multipass: fix doc typo

--- tests/integration/multipass/conftest.py
+++ tests/integration/multipass/conftest.py
@@ -12,7 +12,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""Fixtures for MULTIPASS integration tests."""
+"""Fixtures for Multipass integration tests."""
 import pathlib
 import random
 import string
