efe279046a3de6d5a6c5cba0d066f87839afcc28
Chris Patterson <chris.patterson@canonical.com>
2021-04-14 13:44:52 -0400

    lxd: add project helpers to create/delete projects
    Signed-off-by: Chris Patterson <chris.patterson@canonical.com>

---
+++ craft_providers/lxd/project.py
@@ -0,0 +1,85 @@
+# Copyright 2021 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Project helper utilities."""
+import logging
+
+from .lxc import LXC
+
+logger = logging.getLogger(__name__)
+
+
+def create_with_default_profile(
+    *,
+    lxc: LXC,
+    project: str,
+    profile: str = "default",
+    profile_project: str = "default",
+    remote: str = "local",
+) -> None:
+    """Create a project with a valid default profile.
+
+    LXD does not set a valid profile on newly created projects.  This will
+    create a project and set the profile to match the specified profile,
+    typically the default.
+
+    :param project: Name of project to create.
+    :param remote: Name of remote.
+    :param profile_name: Name of profile to copy.
+
+    :raises LXDError: on unexpected error.
+    """
+    lxc.project_create(project=project, remote=remote)
+
+    # Retrieve config from specified (default) project profile.
+    config = lxc.profile_show(profile=profile, project=profile_project, remote=remote)
+
+    # Set config.
+    lxc.profile_edit(profile="default", project=project, config=config, remote=remote)
+
+
+def purge(*, lxc: LXC, project: str, remote: str = "local") -> None:
+    """Purge a project including its instances and images.
+
+    The lxc command does not provide a straight-forward option to purge a
+    project.  This helper will purge anything related to a specified one.
+
+    :param project: Name of project to delete.
+    :param remote: Name of remote.
+
+    :raises LXDError: on unexpected error.
+    """
+    projects = lxc.project_list(remote=remote)
+    if project not in projects:
+        logger.debug("Attempted to purge non-existent project '%s'.", project)
+        return
+
+    # Cleanup any outstanding instance_names.
+    for instance_name in lxc.list_names(project=project, remote=remote):
+        logger.debug("Deleting instance_name '%s'.", instance_name)
+        lxc.delete(
+            instance_name=instance_name,
+            project=project,
+            remote=remote,
+            force=True,
+        )
+
+    # Cleanup any outstanding images.
+    for image in lxc.image_list(project=project):
+        logger.debug("Deleting image '%s'.", image)
+        lxc.image_delete(image=image["fingerprint"], project=project, remote=remote)
+
+    # Cleanup project.
+    logger.debug("Deleting project '%s'.", project)
+    lxc.project_delete(project=project, remote=remote)
