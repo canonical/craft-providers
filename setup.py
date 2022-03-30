8275f65d52f3627004124ce1a08c2974b9b0c4a6
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2022-03-30 09:50:36 -0300

    Bump version: 1.1.0 → 1.1.1

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ extras_requires = {
 
 setup(
     name="craft-providers",
-    version="1.1.0",
+    version="1.1.1",
     description="Craft provider tooling",
     long_description=readme,
     long_description_content_type="text/markdown",
