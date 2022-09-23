0570f38e52c45e3dae8e954d8b9e0783544974bd
Callahan Kovacs <callahan.kovacs@canonical.com>
2022-09-23 10:12:33 -0500

    tests: test ubuntu 22.04 images
    ubuntu 22.04 images are available for lxd and multipass, so
    integration tests for these images are enabled.
    
    Signed-off-by: Callahan Kovacs <callahan.kovacs@canonical.com>

--- tests/integration/multipass/test_launch.py
+++ tests/integration/multipass/test_launch.py
@@ -45,8 +45,7 @@ def core20_instance(instance_name):
     [
         (bases.BuilddBaseAlias.BIONIC, "snapcraft:core18"),
         (bases.BuilddBaseAlias.FOCAL, "snapcraft:core20"),
-        # FIXME: enable after image is available
-        # (bases.BuilddBaseAlias.JAMMY, "snapcraft:core22"),
+        (bases.BuilddBaseAlias.JAMMY, "snapcraft:core22"),
     ],
 )
 def test_launch(instance_name, alias, image_name):
