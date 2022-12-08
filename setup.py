51d907c21f9fbc1ceea93e615a76b8fb17f607c6
Callahan <callahan.kovacs@canonical.com>
2022-12-08 11:42:24 -0600

    Bump version: 1.6.1 → 1.6.2 (#186)

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ extras_requires = {
 
 setup(
     name="craft-providers",
-    version="1.6.1",
+    version="1.6.2",
     description="Craft provider tooling",
     long_description=readme,
     long_description_content_type="text/markdown",
