705ce0eea1abbe71956d847db5ccb4ad9a2e0997
Callahan Kovacs <callahan.kovacs@canonical.com>
2023-02-16 13:07:53 -0600

    lint: remove pylint useless suppression "too-many-lines"
    Signed-off-by: Callahan Kovacs <callahan.kovacs@canonical.com>

--- craft_providers/lxd/lxc.py
+++ craft_providers/lxd/lxc.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2021-2022 Canonical Ltd.
+# Copyright 2021-2023 Canonical Ltd.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -30,9 +30,6 @@ from craft_providers import errors
 from .errors import LXDError
 
 logger = logging.getLogger(__name__)
-
-
-# pylint: disable=too-many-lines
 
 
 class StdinType(enum.Enum):
