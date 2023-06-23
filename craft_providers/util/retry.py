48e857b34eb65bdbfc535dbf169be29aae44d9ce
Alex Lowe <alex.lowe@canonical.com>
2023-06-22 21:40:04 -0400

    base: retry retrieving /etc/os-release

--- craft_providers/util/retry.py
+++ craft_providers/util/retry.py
@@ -20,7 +20,7 @@ from __future__ import annotations
 import time
 from typing import TYPE_CHECKING, TypeVar
 
-if TYPE_CHECKING:
+if TYPE_CHECKING:  # pragma: no cover
     from collections.abc import Callable
 
 T = TypeVar("T")
