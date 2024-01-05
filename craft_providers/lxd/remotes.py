f494581728a8c7355a839af3c1add4981f49d561
Callahan <callahan.kovacs@canonical.com>
2024-01-05 12:17:28 -0600

    Update craft_providers/lxd/remotes.py

--- craft_providers/lxd/remotes.py
+++ craft_providers/lxd/remotes.py
@@ -146,7 +146,7 @@ _PROVIDER_BASE_TO_LXD_REMOTE_IMAGE: Dict[Enum, RemoteImage] = {
     ),
     ubuntu.BuilddBaseAlias.NOBLE: RemoteImage(
         image_name="core24",
-        remote_name=DAILY_REMOTE_NAME,
+        remote_name=BUILDD_DAILY_REMOTE_NAME,
         remote_address=DAILY_REMOTE_ADDRESS,
         remote_protocol=ProtocolType.SIMPLESTREAMS,
     ),
