37b0cf79dcd0944cdbac4d44bbdf06061e24d1d0
Callahan <callahan.kovacs@canonical.com>
2024-02-28 12:41:20 -0600

    chore(bases): bump compatibility tag to v7 (#513)
    Signed-off-by: Callahan Kovacs <callahan.kovacs@canonical.com>

--- tests/unit/bases/test_ubuntu_buildd.py
+++ tests/unit/bases/test_ubuntu_buildd.py
@@ -41,7 +41,7 @@ from tests.unit.conftest import DEFAULT_FAKE_CMD
 def mock_load(mocker):
     return mocker.patch(
         "craft_providers.instance_config.InstanceConfiguration.load",
-        return_value=InstanceConfiguration(compatibility_tag="buildd-base-v6"),
+        return_value=InstanceConfiguration(compatibility_tag="buildd-base-v7"),
     )
 
 
@@ -146,7 +146,7 @@ def mock_get_os_release(mocker):
     ],
 )
 @pytest.mark.parametrize(
-    ("tag", "expected_tag"), [(None, "buildd-base-v6"), ("test-tag", "test-tag")]
+    ("tag", "expected_tag"), [(None, "buildd-base-v7"), ("test-tag", "test-tag")]
 )
 def test_setup(
     fake_process,
@@ -624,7 +624,7 @@ def test_ensure_image_version_compatible_failure(fake_executor, monkeypatch):
         base_config._ensure_instance_config_compatible(executor=fake_executor)
 
     assert exc_info.value == BaseCompatibilityError(
-        "Expected image compatibility tag 'buildd-base-v6', found 'invalid-tag'"
+        "Expected image compatibility tag 'buildd-base-v7', found 'invalid-tag'"
     )
 
 
@@ -1165,7 +1165,7 @@ def test_update_setup_status(fake_executor, mock_load, status):
     assert fake_executor.records_of_push_file_io == [
         {
             "content": (
-                "compatibility_tag: buildd-base-v6\n"
+                "compatibility_tag: buildd-base-v7\n"
                 f"setup: {str(status).lower()}\n".encode()
             ),
             "destination": "/etc/craft-instance.conf",
@@ -1274,7 +1274,7 @@ def test_warmup_overall(
     environment, fake_process, fake_executor, mock_load, mocker, cache_path
 ):
     mock_load.return_value = InstanceConfiguration(
-        compatibility_tag="buildd-base-v6", setup=True
+        compatibility_tag="buildd-base-v7", setup=True
     )
 
     alias = ubuntu.BuilddBaseAlias.JAMMY
@@ -1323,7 +1323,7 @@ def test_warmup_overall(
 
 def test_warmup_bad_os(fake_process, fake_executor, mock_load):
     mock_load.return_value = InstanceConfiguration(
-        compatibility_tag="buildd-base-v6", setup=True
+        compatibility_tag="buildd-base-v7", setup=True
     )
     base_config = ubuntu.BuilddBase(
         alias=ubuntu.BuilddBaseAlias.JAMMY,
@@ -1348,7 +1348,7 @@ def test_warmup_bad_os(fake_process, fake_executor, mock_load):
 
 def test_warmup_bad_instance_config(fake_process, fake_executor, mock_load):
     mock_load.return_value = InstanceConfiguration(
-        compatibility_tag="buildd-base-v6", setup=True
+        compatibility_tag="buildd-base-v7", setup=True
     )
     alias = ubuntu.BuilddBaseAlias.JAMMY
     base_config = ubuntu.BuilddBase(
@@ -1377,7 +1377,7 @@ def test_warmup_bad_instance_config(fake_process, fake_executor, mock_load):
 def test_warmup_not_setup(setup, fake_process, fake_executor, mock_load):
     """Raise a BaseConfigurationError if the instance is not setup."""
     mock_load.return_value = InstanceConfiguration(
-        compatibility_tag="buildd-base-v6", setup=setup
+        compatibility_tag="buildd-base-v7", setup=setup
     )
     alias = ubuntu.BuilddBaseAlias.JAMMY
     base_config = ubuntu.BuilddBase(
@@ -1405,7 +1405,7 @@ def test_warmup_not_setup(setup, fake_process, fake_executor, mock_load):
 
 def test_warmup_never_ready(fake_process, fake_executor, mock_load):
     mock_load.return_value = InstanceConfiguration(
-        compatibility_tag="buildd-base-v6", setup=True
+        compatibility_tag="buildd-base-v7", setup=True
     )
     alias = ubuntu.BuilddBaseAlias.JAMMY
     base_config = ubuntu.BuilddBase(
@@ -1437,7 +1437,7 @@ def test_warmup_never_ready(fake_process, fake_executor, mock_load):
 
 def test_warmup_never_network(fake_process, fake_executor, mock_load):
     mock_load.return_value = InstanceConfiguration(
-        compatibility_tag="buildd-base-v6", setup=True
+        compatibility_tag="buildd-base-v7", setup=True
     )
     alias = ubuntu.BuilddBaseAlias.JAMMY
     base_config = ubuntu.BuilddBase(
