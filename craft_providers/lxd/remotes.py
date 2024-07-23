b9c60b6ef6a420f27f5a47d1516262b82d04409b
Callahan Kovacs <callahan.kovacs@canonical.com>
2024-07-23 13:29:02 -0500

    chore!: remove deprecated `configure_buildd_image_remote()` function
    Signed-off-by: Callahan Kovacs <callahan.kovacs@canonical.com>

--- craft_providers/lxd/remotes.py
+++ craft_providers/lxd/remotes.py
@@ -19,7 +19,6 @@
 """Manages LXD remotes and provides access to remote images."""
 
 import logging
-import warnings
 from dataclasses import dataclass
 from enum import Enum
 from typing import Dict, Union
@@ -196,29 +195,3 @@ def get_remote_image(provider_base: Union[Base, str]) -> RemoteImage:
         )
 
     return image
-
-
-def configure_buildd_image_remote(lxc: LXC = LXC()) -> str:
-    """Configure the default buildd image remote.
-
-    This is a deprecated function to maintain the existing API. It will be
-    removed with the release of craft-providers 2.0.
-    Applications should call `get_remote_image()` and `RemoteImage.add_remote()`.
-
-    :param lxc: LXC client.
-
-    :returns: Name of remote to pass to launcher.
-    """
-    warnings.warn(
-        message=(
-            "configure_buildd_image_remote() is deprecated. "
-            "Use get_remote_image() and RemoteImage.add_remote()."
-        ),
-        category=DeprecationWarning,
-    )
-    # configure the buildd remote for core22
-    base = ubuntu.BuilddBase(alias=ubuntu.BuilddBaseAlias.JAMMY)
-    image = get_remote_image(base)
-    image.add_remote(lxc)
-
-    return BUILDD_RELEASES_REMOTE_NAME
