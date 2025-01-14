077854fe5ba30ba8db54edcf392cba5ad8cedc28
Matt Culler <matt.culler@canonical.com>
2025-01-14 16:27:04 -0500

    fix: unit tests for changed interface

--- tests/unit/test_hookutil.py
+++ tests/unit/test_hookutil.py
@@ -75,7 +75,7 @@ def assert_instances_deleted(helper, instances):
     """Transform json list to instance calls for passing to assert_has_calls."""
     helper.delete_instance.assert_has_calls(
         [
-            call(LXDInstance.unmarshal(instance, project_name=PROJECT_NAME))
+            call(LXDInstance.unmarshal(instance))
             for instance in instances
         ],
         any_order=True,
