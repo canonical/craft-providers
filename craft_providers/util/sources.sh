a0527e2017a40b8fa83b0acb0ea2b7e08b4b0d84
Callahan Kovacs <callahan.kovacs@canonical.com>
2025-07-17 17:04:46 -0500

    fix: update ports.ubuntu.com sources
    Signed-off-by: Callahan Kovacs <callahan.kovacs@canonical.com>

--- craft_providers/util/sources.sh
+++ craft_providers/util/sources.sh
@@ -7,7 +7,11 @@ set -e
 for file in /etc/apt/sources.list /etc/apt/sources.list.d/ubuntu.sources; do
   if [[ -f "$file" ]]; then
     echo "Updating '${file}'."
-    sed -i -e "s/security.ubuntu.com/old-releases.ubuntu.com/" -e "s/archive.ubuntu.com/old-releases.ubuntu.com/" "${file}"
+    sed -i \
+       -e "s|security.ubuntu.com|old-releases.ubuntu.com|g" \
+       -e "s|archive.ubuntu.com|old-releases.ubuntu.com|g" \
+       -e "s|ports.ubuntu.com/ubuntu-ports|old-releases.ubuntu.com/ubuntu|g" \
+       "${file}"
   else
     echo "'${file}' doesn't exist, skipping."
 fi
