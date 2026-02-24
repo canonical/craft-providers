e22ef36f02af4c973476c6a2d9aa76e42c93f80a
Vova956 <Vladimrvarga2019@gmail.com>
2026-02-24 16:52:31 -0500

    Added abstract method to providers.py

--- craft_providers/provider.py
+++ craft_providers/provider.py
@@ -61,6 +61,13 @@ class Provider(ABC):
     @abstractmethod
     def install_recommendation(self) -> str:
         """Recommended way to install the provider."""
+        
+    @abstractmethod
+    def delete_instances(self, instances: Collection[Executor], *, force: bool = False,) -> None:
+        """Delete the specified instances.
+            :param instances: Instances to delete.
+            :param force: If True, attempt deletion even if instance is running.
+        """
 
     def clean_project_environments(self, *, instance_name: str) -> None:
         """Clean the provider environment.
